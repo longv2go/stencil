9867cd7d2b566f1e62ad29f59112100e1eec465c
Ken Sodemann <ken@ionic.io>
2017-08-24 05:16:41 -0500

    WIP - verify minify call

--- src/compiler/app/test/app-loader.spec.ts
+++ src/compiler/app/test/app-loader.spec.ts
@@ -15,6 +15,12 @@ describe('build-project-files', () => {
   });
 
   describe('inject project', () => {
+    let mockMinify: jest.Mock<any>;
+    beforeEach(() => {
+      mockMinify = jest.fn();
+      mockMinify.mockReturnValue({ diagnostics: [] });  // NOTE: bad idea - typedef has this as optional, but not optional in the code under test...
+      config.sys.minifyJs = mockMinify;
+    });
 
     it('should set the loader arguments', () => {
       const projectLoader = callInjectAppIntoLoader({ componentRegistry: [['my-app', 'MyApp.Module', { Mode1: 'something', Mode2: 'Something Else' }, [], [], 42, 73]] });
@@ -23,27 +29,59 @@ describe('build-project-files', () => {
 
     it('only replaces the magic string', () => {
       mockStencilContent = `(This is bogus text'__STENCIL__APP__'yeah, me too)`;
-      const projectLoader = callInjectAppIntoLoader({});
+      const projectLoader = callInjectAppIntoLoader();
       expect(projectLoader).toBe(`(This is bogus text"MyApp","build/myapp/myapp.core.js","build/myapp/myapp.core.pf.js",[]yeah, me too)`);
+    });
+
+    describe('with minifyJs true', () => {
+      it('calls the minify routine', () => {
+        config.minifyJs = true;
+        callInjectAppIntoLoader();
+        expect(mockMinify.mock.calls.length).toEqual(1);
+      });
+
+      it('returns minified output', () => {
+
+      });
+
+      describe('with diagnostic messages', () => {
+        it('logs the messages', () => {
+
+        });
+
+        it('returns the non-minified data', () => {
+
+        });
+      });
+    });
+
+    describe('with minifyJs falsey', () => {
+      it('does not minify', () => {
+        callInjectAppIntoLoader();
+        config.minifyJs = false;
+        callInjectAppIntoLoader();
+        expect(mockMinify.mock.calls.length).toEqual(0);
+      });
     });
 
   });
 
-  function callInjectAppIntoLoader(params: {
+  function callInjectAppIntoLoader(params?: {
     namespace?: string,
     publicPath?: string,
     coreFileName?: string,
     corePolyfillFileName?: string,
     componentRegistry?: Array<LoadComponentRegistry>
   }) {
-    config.namespace = params.namespace || 'MyApp';
-    config.publicPath = params.publicPath || 'build/';
+    let p = params || {};
+    config.namespace = p.namespace || 'MyApp';
+    config.publicPath = p.publicPath || 'build/';
     return injectAppIntoLoader(
       config,
-      params.coreFileName || 'myapp.core.js',
-      params.corePolyfillFileName || 'myapp.core.pf.js',
+      p.coreFileName || 'myapp.core.js',
+      p.corePolyfillFileName || 'myapp.core.pf.js',
       getAppPublicPath(config),
-      params.componentRegistry || [],
+      p.componentRegistry || [],
       mockStencilContent
     );
   }
